a75e0372092ae2bd7adf403c1edf2bb26167cb3c
SinghReena <Reena.singh.tsu@gmail.com>
2018-05-01 00:14:26 -0700

    Add files via upload

---
+++ AskTwoPeopleNames.py
@@ -0,0 +1,5 @@
+#AskTwoPeopleNames.py
+name1 = input("What is your mom's name?\n")
+print("My mom's name is " , name1)
+name2 = input("What is your dad's name?\n")
+print("My dad's name is " , name2)
