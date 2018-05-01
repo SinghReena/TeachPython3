58a3830565bd84175035bf37b5732ab1bb6d20ab
SinghReena <Reena.singh.tsu@gmail.com>
2018-05-01 01:43:50 -0700

    Tilted Square Shape

---
+++ TiltedSquare.py
@@ -0,0 +1,13 @@
+#TiltedSquare.py
+import turtle
+
+turtle.left(20)
+
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
