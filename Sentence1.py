9d5c4774150404a07fb65977d52b893f1d32a698
SinghReena <Reena.singh.tsu@gmail.com>
2018-05-01 23:02:41 -0700

    Make sentence by using adjective,noun,verb,animal
    Make sentence by using adjective, noun, verb, animal name 
    Use single quotes (' ') only

---
+++ Sentence1.py
@@ -0,0 +1,8 @@
+#Sentence1.py
+
+adjective = input('enter an adjective: ')
+noun = input('enter a noun: ')
+verb = input('enter a verb ending in -ed: ')
+animal = input('enter an animal: ')
+print('Sentence:')
+print('The', adjective, noun, verb, 'over the lazy black', animal + '.')
