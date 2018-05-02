2b8361f3e5c7f6de76679b9cbbef573c20aafe9a
SinghReena <Reena.singh.tsu@gmail.com>
2018-05-01 22:55:06 -0700

    Make sentence by using adj. , noun and verb

---
+++ Sentence.py
@@ -0,0 +1,7 @@
+# Sentence.py
+adjective = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb ending in -ed: ")
+print("Your full sentence:")
+print("The", adjective, noun, verb, "over the lazy white cat.")
+
