9bf73224c5095ec5fe831bdfc8cbfeb3d482d781
SinghReena <Reena.singh.tsu@gmail.com>
2018-05-04 23:10:00 -0700

    Color Spiral (Using attribute width)

---
+++ UseColors/ColorSpiral.py
@@ -0,0 +1,12 @@
+# ColorSpiral.py
+import turtle
+t = turtle.Pen()
+turtle.bgcolor("black")
+# You can choose between 2 and 6 sides for some cool shapes!
+Totalsides = 6
+colors = ["red", "yellow", "blue", "orange", "green", "purple"]
+for x in range(360):
+    t.pencolor(colors[x%Totalsides])
+    t.forward(x * 3/Totalsides + x)
+    t.left(360/Totalsides + 1)
+    t.width(x*Totalsides/200)
