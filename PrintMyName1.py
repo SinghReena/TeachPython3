21eb8877faa7e88a4c596f3d2b54708dc043c2e6
SinghReena <Reena.singh.tsu@gmail.com>
2018-05-06 23:30:30 -0700

    Print their name always in a new line

---
+++ PrintMyName1.py
@@ -0,0 +1,8 @@
+# PrintMyName1.py - prints a screen full of the user's name 
+# Ask the user for their name
+name = input("Could you please tell your name: ")
+# Print their name 100 times
+for x in range(100):
+    # Print their name always in a new line
+    
+    print(name)
