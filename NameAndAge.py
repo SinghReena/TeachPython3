1d609a2685fc93e311b3fbc7b33b7b6ea138ca0d
SinghReena <Reena.singh.tsu@gmail.com>
2018-05-07 21:47:23 -0700

    Print Name and age using variables and input

---
+++ NameAndAge.py
@@ -0,0 +1,10 @@
+#NameAndAge.py
+
+teacher_name = "Reena"
+student_name = "Natalie"
+student_age = 4
+student_name1 = input("What is your name? ")
+student_age1  = input("How old are you? ")
+print("My name is", student_name, ", and I am", student_age, "years old.")
+print("Your name is", student_name1, ", and you are", student_age1, ".")
+print("Thank you for teaching python,My first programming teacher ,", teacher_name, "!")
